9dbd7980aaa6b168b6f678bf2984e118abb7a9ca
com <com@192.168.123.100>
2022-06-08 01:13:47 +0900

    fix : noticeWrite fix

--- TeenCoder/src/main/java/com/ez/teen/common/file/FileUtils.java
+++ TeenCoder/src/main/java/com/ez/teen/common/file/FileUtils.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.ez.teen.board.model.BoardModel;
+import com.ez.teen.notice.model.NoticeModel;
 
 @Component("fileUtils")
 public class FileUtils {
@@ -67,6 +68,50 @@ public class FileUtils {
 		}
 		return list;
 	}
+	
+	public List<Map<String, Object>> parseInsertFileInfo(NoticeModel noticeModel,MultipartHttpServletRequest mpRequest) throws Exception{
+		
+			Iterator<String> iterator = mpRequest.getFileNames();
+		
+			MultipartFile multpartFile = null;
+			String originalFileName = null;
+			String originalFileExtension = null;
+			String storedFileName = null;
+		
+			List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		
+			Map<String, Object> listMap = null;
+		
+			int noti_no = noticeModel.getNoti_no();
+				
+			File file = new File(filePath);
+			if(file.exists() == false) {
+				file.mkdirs();
+			} 
+		
+			while (iterator.hasNext()) {
+				multpartFile = mpRequest.getFile(iterator.next());
+			
+				if(multpartFile.isEmpty() == false) {
+					originalFileName = multpartFile.getOriginalFilename();
+					originalFileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+				
+					storedFileName = getRandomString() + originalFileExtension;
+					Date date = new Date();
+				
+					file = new File(filePath + storedFileName);
+					multpartFile.transferTo(file);
+					listMap = new HashMap<String, Object>();
+					listMap.put("BOARD_NO", noti_no);
+					listMap.put("ORG_FILE_NAME", originalFileName);
+					listMap.put("STORED_FILE_NAME", storedFileName);
+					listMap.put("FILE_SIZE", multpartFile.getSize());
+					listMap.put("FILE_DATE", date);
+					list.add(listMap);
+				}
+			}
+			return list;
+		}
 	
 	public List<Map<String, Object>> parseUpdateFileInfo(BoardModel boardModel, String[] files,String[] fileNames, MultipartHttpServletRequest mpRequest) throws Exception {
 		
