38d67c9aecdb473b0f739dcb97b48306ea1edc5e
kwackbackE <ehdgur0591@gmail.com>
2022-06-10 21:35:59 +0900

    [Fix] adminBoard.js 분리
    공통 기능이였던 조회기능을 adminBoard.js파일로 분리

---
+++ TeenCoder/src/main/webapp/resources/js/admin/adminBoard.js
@@ -0,0 +1,12 @@
+function searchBoard() {
+	var keyword = document.getElementById('keyword').value;
+	var selectedElement = document.getElementById("selectBoxTest");    
+	var optionVal = selectedElement.options[selectedElement.selectedIndex].value; 
+	console.log(keyword);
+	// 검색 내용 확인 
+	if (optionVal == '' || keyword == "") {
+		alert('검색 설정을 확인해주세요.');
+		return false;
+	}
+	location.href = '/teen/admin/adminBoard?search='+optionVal+'&keyword='+ keyword;
+}
