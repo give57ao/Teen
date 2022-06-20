30007f10e44809b90f0f71fd602a06cbb5dd16f6
Chori <cjfcksgml@gmail.com>
2022-06-21 02:07:17 +0900

    feat : 회원가입 이메일 인증 기능 추가

--- TeenCoder/src/main/java/com/ez/teen/member/controller/MemberJoinController.java
+++ TeenCoder/src/main/java/com/ez/teen/member/controller/MemberJoinController.java
@@ -1,16 +1,22 @@
 package com.ez.teen.member.controller;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ez.teen.member.VerifyRecaptcha;
@@ -28,6 +34,9 @@ public class MemberJoinController {
 	@Autowired
     PasswordEncoder passwordEncoder;
 
+	@Autowired
+	JavaMailSender mailSender; 
+	
 	private static final Logger log = LoggerFactory.getLogger(MemberJoinController.class);
 
 	// 회원가입 화면
@@ -111,5 +120,34 @@ public class MemberJoinController {
     		System.out.println("캡차 로그인 에러");
 	        return -1; //에러
 	    }
+	}
+	
+	@GetMapping("/mailCheck")
+	@ResponseBody
+	public String mailCheck(@RequestParam("member_email") String member_email)throws Exception{
+		int serti = (int)((Math.random()*(99999 - 10000 + 1)) + 10000);
+		System.out.println("member_email = " + member_email);
+		String from = "teencoder@naver.com";
+		String to = member_email;
+		String title = "회원가입시 필요한 인증번호 입니다.";
+		String content = "[인증번호]" + serti + "입니다. <br/> 인증번호 확인란에 기입해주십시오.";
+		String num = "";
+		
+		try {
+			MimeMessage mail = mailSender.createMimeMessage();
+			MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
+			
+			mailHelper.setFrom(from);
+			mailHelper.setTo(to);
+			mailHelper.setSubject(title);
+			mailHelper.setText(content, true);
+			
+			mailSender.send(mail);
+	        num = Integer.toString(serti);
+			
+		}catch(Exception e) {
+			num = "error";
+		}
+		return num;
 	}
 }
